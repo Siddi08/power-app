9311875ef31a894ed0e0a0c454a1bc1220fcfb61
Siddi08 <s.pasupati0810@gmail.com>
2026-04-29 15:22:06 +0930

    Initial commit: POWER PWA training tracker

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'power-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800;900&family=Barlow:wght@400;500;600&display=swap'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS.slice(0, 4))));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached || new Response('Offline', {status: 503}));
+    })
+  );
+});
